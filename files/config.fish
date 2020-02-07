51dc9746e08604a28fc02d4d2de5d65036f1e2ff
Julian Panzini <jpanzini@neuronicworks.com>
2020-02-07 13:40:54 -0500

    Added abbr

--- files/config.fish
+++ files/config.fish
@@ -1,3 +1,43 @@
+# Abbriviations
+abbr -a -U -- .. 'cd ../'
+abbr -a -U -- ... 'cd ../../'
+abbr -a -U -- .... 'cd ../../../'
+abbr -a -U -- ..... 'cd ../../../../'
+abbr -a -U -- add 'git add -A'
+abbr -a -U -- c clear
+abbr -a -U -- cdd 'cd -'
+abbr -a -U -- g. 'git checkout .'
+abbr -a -U -- gb 'git branch'
+abbr -a -U -- gc 'git checkout'
+abbr -a -U -- gc-a 'git commit --amend'
+abbr -a -U -- gc-b 'git checkout -b'
+abbr -a -U -- gcd 'git checkout dev'
+abbr -a -U -- gcm 'git checkout master'
+abbr -a -U -- gcom 'git commit -m'
+abbr -a -U -- gdt 'git tag -d'
+abbr -a -U -- gdtr 'git push -d origin'
+abbr -a -U -- gf 'git fetch'
+abbr -a -U -- gfs 'git fetch && git status'
+abbr -a -U -- gmd 'git merge dev'
+abbr -a -U -- gmm 'git merge master'
+abbr -a -U -- gmt 'git mergetool && rm *.orig'
+abbr -a -U -- gnb 'git checkout -b'
+abbr -a -U -- gp 'git push'
+abbr -a -U -- gpl 'git pull'
+abbr -a -U -- gpt 'git push --tags'
+abbr -a -U -- gpuo 'git push -u origin'
+abbr -a -U -- gr-c 'git rm --cached'
+abbr -a -U -- gr-h 'git reset --hard HEAD'
+abbr -a -U -- gs 'git status'
+abbr -a -U -- gt 'git tag'
+abbr -a -U -- jb 'jekyll b'
+abbr -a -U -- js 'jekyll s'
+abbr -a -U -- ll 'ls -la'
+abbr -a -U -- projects 'cd /mnt/c/Projects/'
+abbr -a -U -- rmf 'rm -rf'
+abbr -a -U -- x exit
+
+# Prompt Settings
 set -g theme_title_display_user yes
 set -g theme_display_user yes
 set -g theme_powerline_fonts yes
